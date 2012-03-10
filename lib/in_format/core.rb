727c8337f18dfd423e3e8c26db596708488f580a
Cyrus Farajpour <smoils@gmail.com>
2012-03-09 23:57:19 -0800

    Remove commented out code from core.rb.

--- lib/in_format/core.rb
+++ lib/in_format/core.rb
@@ -4,11 +4,9 @@ module InFormat
 
     if opts[:setter]
       if opts[:use_accessor]
-        # alias_method "_#{attribute}=".to_sym, "#{attribute}=".to_sym
         original_setter = self.instance_method("#{attribute}=")
 
         define_method("#{attribute}=") do |value|
-          # self.send "_#{attribute}=", opts[:setter].call(value)
           original_setter.bind(self).call(opts[:setter].call(value))
         end
       else
@@ -20,15 +18,12 @@ module InFormat
 
     if opts[:getter]
       if opts[:use_accessor]
-        # alias_method "_#{attribute}".to_sym, "#{attribute}".to_sym
         original_getter = self.instance_method(attribute)
 
         define_method("#{attribute}") do |raw = false|
           if raw
-            # self.send "_#{attribute}"
             original_getter.bind(self).call
           else
-            # opts[:getter].call(self.send "_#{attribute}")
             opts[:getter].call(original_getter.bind(self).call)
           end
         end
